ee958d1eb656811f96e05d1a0dbf41caab32cd6b
Rishabh Gupta <34839714+rishabhg17@users.noreply.github.com>
2021-10-08 22:17:46 +0530

    Update Themes.js

--- Themes.js
+++ Themes.js
@@ -87,4 +87,13 @@ const themes = [
 		textColor: [255, 152, 0],
 		paddleColor: [255, 193, 7],
 	},
+	{
+		ballColor: [155, 152, 0],
+		trailType: 'decreasing',
+		trailLength: 20,
+		trailColor: [155, 193, 7],
+		background: [11],
+		textColor: [155, 152, 0],
+		paddleColor: [155, 193, 7],
+	},
 ];
